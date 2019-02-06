69bc483cc33ed52be14df6d1483368f960c47a56
Gopish Monga <gopishmonga@gmail.com>
2019-02-06 21:56:16 +0530

    level width array with bfs for linked list

--- levelwidth/index.js
+++ levelwidth/index.js
@@ -11,6 +11,25 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const queue = [root,null];
+    const levelWidthArray = [];
+    let counter = 0;
+    while(queue.length){
+        const node = queue.shift();
+        if(node){
+            counter++;
+            queue.push(...node.children);
+        }
+        else{
+            if(counter > 0){
+                queue.push(null);
+                levelWidthArray.push(counter);
+                counter = 0;
+            }
+        }
+    }
+    return levelWidthArray;
+}
 
 module.exports = levelWidth;
